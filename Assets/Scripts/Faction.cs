3cd5b9302a166b38a58bccdf39bc22ccfac218d5
Nate Mendoza <nmendoza82rg@aol.com>
2019-05-27 13:30:14 -0700

    Big dump
    Forgot to do this repo but i made a map with 9 locations i created about 8 scripts but they dont do anything getting all the pieces aligned first here. added 3 asset packs. one was of bridges that needed a special asset pack for substances
    added a couple of terrain items as well

---
+++ Assets/Scripts/Faction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Faction : MonoBehaviour
+{
+
+    public string factionName;
+    public string governmentType;
+    public string[] typesOfGovernments = { "Monarchy", "Demarchy", "Confederation", "Anarchy"};
+    public int numberOfArmies;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour
+{
+    public string thisLocationName;
+
+    public Location[] nextLocationsToTravelTo;
+    [Range(1,5)]
+    public int locationPowerFactor;
+    public Faction currentlyOccupiedBy;
+
+    public MilitaryLeader militaryRuler;
+
+    public LocalSupplier marketCharacter;
+
+    public TownFolk infoSpeakers;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
